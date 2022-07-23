ef5fa9885c703183ef120b77a05d3a7581c93d99
nizam <nizam.fci@gmail.com>
2022-07-24 01:06:00 +0600

    second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>This is my first react js project. I'm hard working to learn react js an </h1>
+
   </React.StrictMode>
 );
 
